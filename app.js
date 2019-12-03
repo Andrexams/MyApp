a9cde2284f204b1947082f5d4eec6f0577b6108d
Andrexams <35140343+Andrexams@users.noreply.github.com>
2019-12-03 20:34:04 -0300

    Update  app.js
    Atualizando o app value sigla.

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ angular.module('jobsModule',[]);
 //registrar main module
 var app = angular.module('myApp',['jobsModule']);
 
-app.value('sigla','My Angular first App');
+app.value('sigla','MyApp');
 app.constant('appid','APPMARTINS2019');
